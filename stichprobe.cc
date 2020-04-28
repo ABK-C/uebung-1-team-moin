eb07597987928447075ce3c04a55d6590db8d7b8
JasminBecks <jbecks@physnet.uni-hamburg.de>
2020-04-28 10:10:28 +0000

    Aufgabe 1

--- stichprobe.cc
+++ stichprobe.cc
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<fstream>
+#include<cmath>
 using namespace std;
 
 int main() {
@@ -9,5 +10,23 @@ double summand = 0;
 for(double n=1; n<=234; ++n) {
   summand += n;
 }
-cout << summand/234 << endl;
+double mittelwert = summand/234;
+
+cout << "Mittelwert = " << mittelwert << endl;
+
+
+double sum = 0;
+
+for(double n=1; n<=234; ++n) {
+  sum += (n-mittelwert)*(n-mittelwert);
+}
+
+double varianz = sum/234;
+
+cout << "Varianz = " << varianz << endl;
+
+
+double sigma = sqrt(varianz);
+
+cout << "Standardabweichung = " << sigma << endl;
 }
