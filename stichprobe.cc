0427c8a29e06a483a7fd86b04d4fe15bfec5d20d
JasminBecks <jbecks@physnet.uni-hamburg.de>
2020-05-02 10:20:10 +0000

    zweiter Versuch

--- stichprobe.cc
+++ stichprobe.cc
@@ -4,11 +4,14 @@
 using namespace std;
 
 int main() {
-ifstream fin("datensumme.txt");
+ifstream fin1("datensumme.txt");
+ifstream fin2("datensumme.txt");
 double summand = 0;
 
 for(double n=1; n<=234; ++n) {
-  summand += n;
+  double a_n;
+  fin1 >> a_n;
+  summand += a_n;
 }
 double mittelwert = summand/234;
 
@@ -18,7 +21,9 @@ cout << "Mittelwert = " << mittelwert << endl;
 double sum = 0;
 
 for(double n=1; n<=234; ++n) {
-  sum += (n-mittelwert)*(n-mittelwert);
+  double a_n;
+  fin2 >> a_n;
+  sum += (a_n-mittelwert)*(a_n-mittelwert);
 }
 
 double varianz = sum/234;
@@ -29,4 +34,7 @@ cout << "Varianz = " << varianz << endl;
 double sigma = sqrt(varianz);
 
 cout << "Standardabweichung = " << sigma << endl;
+
+fin1.close();
+fin2.close();
 }
